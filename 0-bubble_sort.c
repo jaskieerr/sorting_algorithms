3b8cabc598bf73e58f42b442cd0aabcc2f905d02
jaskieerr <nizar.zairat@gmail.com>
2023-11-21 04:12:58 -0800

    betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,3 +1,4 @@
+
 #include "sort.h"
 
 /**
@@ -21,24 +22,23 @@ void switch_val(int *val, int *valu)
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, len = size;
-    bool swapped = false;
+	size_t i, len = size;
+	bool swapped = false;
 
-    if (array == NULL || size < 2)
-        return;
-
-    while (!swapped)
-    {
-        swapped = true;
-        for (i = 0; i < len - 1; i++)
-        {
-            if (array[i] > array[i + 1])
-            {
-                switch_val(array + i, array + i + 1);
-                print_array(array, size);
-                swapped = false;
-            }
-        }
-        len--;
-    }
+	if (array == NULL || size < 2)
+		return;
+	while (!swapped)
+	{
+		swapped = true;
+		for (i = 0; i < len - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				switch_val(array + i, array + i + 1);
+				print_array(array, size);
+				swapped = false;
+			}
+		}
+		len--;
+	}
 }
